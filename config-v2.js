ba827ec987aea7e434dc45d7966318fb5d7e1e26
BRONE <sanvithawathsana1@gmail.com>
2025-08-31 23:27:17 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
 const DATABASE = "mongodb://mongo:SWvotdARslcGLEqihrkCpaLCintmcvLd@shuttle.proxy.rlwy.net:43137" // මෙතනට mongodb,mysql,postgresql or github token එකක් enter කරන්න මේ "" දෙක මැදට. (Enter a mongodb,mysql,postgresql or github token here between these two "".)
 const BOT_NUMBER = "94724049182" // ඔයාගෙ whatsapp number එක. (your whatsapp number)
-const SESSION_ID = "PRABATH-MD~s19g6G0e8YHLShe" // ඔයාගෙ session id එක මේකෙන් ගන්න - https://prabathmd-official.vercel.app
+const SESSION_ID = "PRABATH-MD~SFBXxMeaKg2l6D0" // ඔයාගෙ session id එක මේකෙන් ගන්න - https://prabathmd-official.vercel.app
 
 
 // ඉහත ඒව "" මැදට එකතු කරන්න වෙනත් කිසිම දෙයක් වෙනස් නොකරන්න ❌. (Add "" in the middle of the above and ❌ don't change anything else.)
